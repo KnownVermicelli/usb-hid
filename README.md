52c7f1edf89ecb30cebfe342b1603fac9fa2453b
Adam Winiarczuk <adam.winiarczuk@gmail.com>
2018-04-06 17:01:50 +0200

    Change to rtfm framework

--- README.md
+++ README.md
@@ -20,3 +20,13 @@ at your option.
 Unless you explicitly state otherwise, any contribution intentionally submitted
 for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
 dual licensed as above, without any additional terms or conditions.
+
+## Developing tools
+
+You will need:
+- something to flash stm32. I'm using stm32flash: 	https://git.code.sf.net/p/stm32flash/code
+- linux tools for building stm32 stuff. You can find instruction on japaric blog in "rust your arm microcontroller".
+
+Notes:
+- PA11 (USBDM) and PA12 (USBDP) are usb pins.
+- https://github.com/ah-/anne-key seems not to set any usb pins, so it's using defaults. should check how code in stm32f103xx crate works.
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,102 @@
-//! Blinks an LED
-#![feature(used)]
 #![deny(unsafe_code)]
 #![deny(warnings)]
+#![feature(proc_macro)]
+#![feature(used)]
 #![no_std]
 
 extern crate cortex_m;
-extern crate cortex_m_rt;
-
-#[macro_use(block)]
-extern crate nb;
+extern crate cortex_m_rtfm as rtfm;
 extern crate stm32f103xx_hal as hal;
 
 use hal::prelude::*;
 use hal::stm32f103xx;
-use hal::timer::Timer;
 
-fn main() {
-	let cp = cortex_m::Peripherals::take().unwrap();
-	let dp = stm32f103xx::Peripherals::take().unwrap();
+use cortex_m::peripheral::syst::SystClkSource;
+use rtfm::{app, Threshold};
 
-	let mut flash = dp.FLASH.constrain();
-	let mut rcc = dp.RCC.constrain();
+app! {
+	device: stm32f103xx,
 
-	let clocks = rcc.cfgr.freeze(&mut flash.acr);
+	// Here data resources are declared
+	//
+	// Data resources are static variables that are safe to share across tasks
+	resources: {
+		static ON: bool = false;
+		static LED: hal::gpio::gpioc::PC13<hal::gpio::Output<hal::gpio::PushPull>>;
+	},
 
-	let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
-
-	let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-
-	let mut timer = Timer::syst(cp.SYST, 1.hz(), clocks);
-	loop {
-		block!(timer.wait()).unwrap();
-		led.set_high();
-		block!(timer.wait()).unwrap();
-		led.set_low();
+	// Tasks corresponding to hardware interrupts
+	// See stm32f103xx::Interrupts
+	tasks: {
+		// Here we declare that we'll use the SYS_TICK exception as a task
+		SYS_TICK: {
+			path: sys_tick,
+			resources: [ON, LED],
+		},
+		CAN1_TX: {
+			path: usb_high_priority_interrupt,
+			resources: [ON],
+		},
+		CAN1_RX0: {
+			path: usb_low_priority_interrupt,
+			resources: [ON],
+		},
 	}
 }
 
-#[link_section = ".vector_table.interrupts"]
-#[used]
-static INTERRUPTS: [extern "C" fn(); 240] = [default_handler; 240];
+fn init(mut p: init::Peripherals, r: init::Resources) -> init::LateResources {
+	// `init` can modify all the `resources` declared in `app!`
+	r.ON;
 
-extern "C" fn default_handler() {
-	let mut p = 120;
-	p += 1;
-	drop(p);
+	// power on GPIOC
+	p.device.RCC.apb2enr.modify(|_, w| w.iopcen().enabled());
+
+	// configure PC13 as output
+	p.device.GPIOC.bsrr.write(|w| w.bs13().set());
+	p.device
+		.GPIOC
+		.crh
+		.modify(|_, w| w.mode13().output().cnf13().push());
+
+	// configure the system timer to generate one interrupt every second
+	p.core.SYST.set_clock_source(SystClkSource::Core);
+	p.core.SYST.set_reload(8_000_000); // 1s
+	p.core.SYST.enable_interrupt();
+	p.core.SYST.enable_counter();
+
+	let mut rcc = p.device.RCC.constrain();
+
+	let mut gpioc = p.device.GPIOC.split(&mut rcc.apb2);
+
+	let led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+	init::LateResources { LED: led }
 }
+
+fn idle() -> ! {
+	loop {
+		rtfm::wfi();
+	}
+}
+
+// This is the task handler of the SYS_TICK exception
+//
+// `_t` is the preemption threshold token. We won't use it in this program.
+//
+// `r` is the set of resources this task has access to. `SYS_TICK::Resources`
+// has one field per resource declared in `app!`.
+#[allow(unsafe_code)]
+fn sys_tick(_t: &mut Threshold, mut r: SYS_TICK::Resources) {
+	// toggle state
+	*r.ON = !*r.ON;
+
+	if *r.ON {
+		r.LED.set_high();
+	} else {
+		r.LED.set_low();
+	}
+}
+
+fn usb_high_priority_interrupt(_t: &mut Threshold, mut _r: CAN1_TX::Resources) {}
+
+fn usb_low_priority_interrupt(_t: &mut Threshold, mut _r: CAN1_RX0::Resources) {}
